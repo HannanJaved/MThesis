31442693dddcba3a598fd2a102030edbdc34e749
HannanJaved <82656213+HannanJaved@users.noreply.github.com>
2024-04-10 19:52:50 +0200

    Add files via upload

---
+++ set_device.py
@@ -0,0 +1,10 @@
+import torch
+from torch import backends
+
+def set_device():
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        device = "mps"
+    return device
